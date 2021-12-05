613af7d7dc494c3e6f6f694737e88e9ed737fa0e
Paweł Cieślak <cieslakpawelszc@gmail.com>
2021-12-05 13:58:17 +0100

    coś tam zmieniłem

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const app = express()
-app.get('/' , (req , res) => {
-    res.send('This is my app')
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function (req, res, next) {
+  res.json(req.headers)
 })
 
 app.listen(port)
