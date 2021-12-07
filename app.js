cfc1a52c824aec8044d078066d0d133f1dddbfa6
Paweł Cieślak <cieslakpawelszc@gmail.com>
2021-12-08 00:05:16 +0100

    dsfs

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const { json } = require('express')
 const express = require('express')
 const port = 3000
 const app = express()
-const manifest = 
+
 app.get('/', (req, res) => {
-    console.log(req.body)
+    res.send(req.body, req, req.headers)
     console.log('coś tu przyszło')
 })
+
 app.get('/manifest', function (req, res, next) {
   res.download(__dirname + '/manifestPaczki.xml')
-
-  console.log('1:' + JSON.stringify(req.body))
-  console.log('2:' + JSON.stringify(req.params))
-  console.log('3:' + JSON.stringify(req.body)) 
+  console.log('manifest został pobrany')
 })
+
 app.get('/paczki', (req, res) => {
     res.send('<html><body><p>Joł joł joł tu tede</p></body></html>')
 })
