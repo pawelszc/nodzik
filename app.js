53f446ce7e62a91329425b9905d3260ed96474b1
Paweł Cieślak <cieslakpawelszc@gmail.com>
2022-05-17 21:21:08 +0200

    lol

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express')
+const sha1 = require('js-sha1')
 const port = 3000
 const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))
 
-app.get('/checker', (req, res) => {
-  console.log(req.body)
-  res.send(req.body)
-  
+
+
+const date = new Date();
+let datka = date.getFullYear() * 1e4 + (date.getMonth() + 1) * 100 + date.getDate() + ''; // "20211124"
+let sha1Password = sha1('IaIpAWeL1987!!')
+let sha1Acces = sha1(datka+sha1Password)
+console.log(sha1Acces)
+
+
+app.get('/', (req, res)=>{
+  res.send(datka)
 })
 
-app.get('/', (req, res) => {
-    res.send(req.body)})
-
-app.post('/', (req, res) => {
-      res.send(req.body)})    
-
-app.get('/manifest', function (req, res, next) {
-  res.download(__dirname + '/manifestPaczki.xml')
+app.get('/manifest',  (req, res, next) => {
+  res.send(__dirname + '/manifestPaczki.xml')
   console.log('manifest został pobrany')
 })
-
-
 app.listen(port)
